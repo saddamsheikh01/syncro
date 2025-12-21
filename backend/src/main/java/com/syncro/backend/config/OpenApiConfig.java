73980e062e71938fb72fae48a9cb7060ee504d60
Riccardo Civiero <riccardocivierobusiness@gmail.com>
2025-12-21 17:22:07 +0100

    first commit

---
+++ backend/src/main/java/com/syncro/backend/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.syncro.backend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Syncro API",
+        description = "API backend Syncro - MVP",
+        version = "v1"
+    ),
+    servers = {
+        @Server(url = "http://localhost:8080", description = "Local")
+    }
+)
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
